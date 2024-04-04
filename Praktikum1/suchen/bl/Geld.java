d35dc063083e69c6a031527988fa04cc0b2aad99
caropaul <carolin.paul@hs-osnabrueck.de>
2024-04-04 10:37:36 +0200

    Ware und Geld

---
+++ Praktikum1/suchen/bl/Geld.java
@@ -0,0 +1,11 @@
+package Praktikum1.suchen.bl;
+
+public class Geld {
+    int euro;
+    int cent;
+
+    public Geld(int euro, int cent){
+        this.euro = euro;
+        this.cent = cent;
+    }
+}
--- Praktikum1/suchen/bl/Ware.java
+++ Praktikum1/suchen/bl/Ware.java
@@ -1,5 +1,17 @@
 package Praktikum1.suchen.bl;
 
 public class Ware {
-    
+
+    private long warennummer;
+    private String name;
+    private Geld preis;
+    private String beschreibung;
+
+    public Ware(long warennummer, String name, Geld preis, String beschreibung){
+        this.warennummer = warennummer;
+        this.name = name;
+        this.preis = preis;
+        this.beschreibung = beschreibung;
+    }
+
 }
