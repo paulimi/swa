2a962f6275d66679d2f1bd3e2b99cc2da63baf8c
miriam <paulmiriam@web.de>
2024-04-11 09:59:18 +0200

    problem gefixt

--- Praktikum1/SimpleShoppingApp.java
+++ Praktikum1/SimpleShoppingApp.java
@@ -6,7 +6,7 @@ import ui.control.MenueControl;
 
 public class SimpleShoppingApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         MenueControl menueControl = new MenueControl();
         menueControl.navigateMenue();
